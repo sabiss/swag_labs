7acda72775865c389bf720dac8a3f15a924b2346
sabiss <sabrinabezsilva@gmail.com>
2026-07-02 10:53:34 -0300

    FEAT: component de tarefa

---
+++ components/tarefaComponent.ts
@@ -0,0 +1,22 @@
+import { Locator } from '@playwright/test'
+
+export class TarefaComponent{
+    readonly tarefa: Locator;
+    readonly checkbox: Locator;
+    readonly botaoDeletar: Locator;
+
+    constructor(tarefa: Locator){
+        this.tarefa = tarefa;
+        this.checkbox = tarefa.getByRole('checkbox');
+        this.botaoDeletar = tarefa.getByRole('button', {name: 'Delete'});
+    }
+
+    async concluirTarefa(){
+        await this.checkbox.check();
+    }
+    
+    async deletarTarefa(){
+        await this.tarefa.hover();
+        await this.botaoDeletar.click();
+    }
+}
