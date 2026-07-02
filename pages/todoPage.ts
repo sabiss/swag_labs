a219a76064beea890fb45e24d8a88a5ef94e69e5
sabiss <sabrinabezsilva@gmail.com>
2026-07-02 12:12:51 -0300

    FIX: falta de click para focar no input

--- pages/todoPage.ts
+++ pages/todoPage.ts
@@ -3,14 +3,21 @@ import { TarefaComponent } from '../components/tarefaComponent';
 
 export class TodoPage{
     readonly page: Page;
+    readonly listaDeTarefas: Locator;
     readonly inputDeTarefa: Locator;
 
     constructor(page: Page){
         this.page = page;
         this.inputDeTarefa = page.getByRole('textbox', {'name': "What needs to be done?"});
+        this.listaDeTarefas = page.getByTestId('todo-item');
+    }
+
+    async acessarPagina(){
+        await this.page.goto("https://demo.playwright.dev/todomvc/#/");
     }
 
     async criarNovaTarefa(mensagemTarefa: string){
+        await this.inputDeTarefa.click();
         await this.inputDeTarefa.fill(mensagemTarefa);
         await this.inputDeTarefa.press('Enter');
     }
