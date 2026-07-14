4945e73cf16f6d943a5854461bdf5339a3d727fa
sabiss <sabrinabezsilva@gmail.com>
2026-07-14 16:11:41 -0300

    REFACTOR: melhorando component

--- components/tarefaComponent.ts
+++ components/tarefaComponent.ts
@@ -1,22 +1,30 @@
 import { Locator, Page } from '@playwright/test'
 
 export class TarefaComponent{
-    readonly tarefa: Locator;
-    readonly checkbox: Locator;
-    readonly botaoDeletar: Locator;
+    readonly page: Page;
+    readonly inputDeTarefa: Locator;
+    readonly texto: string;
 
-    constructor(tarefa: Locator){
-        this.tarefa = tarefa;
-        this.checkbox = tarefa.getByRole('checkbox');
-        this.botaoDeletar = tarefa.getByRole('button', {name: 'Delete'});
+    constructor(page: Page, texto:string){
+        this.page = page;
+        this.inputDeTarefa = page.getByRole('textbox', {'name': "What needs to be done?"});
+        this.texto = texto;
+    }
+
+    async adicionarTarefa(){
+        await this.inputDeTarefa.click();
+        await this.inputDeTarefa.fill(this.texto);
+        await this.inputDeTarefa.press('Enter');
     }
 
     async concluirTarefa(){
-        await this.checkbox.check();
+        const checkBoxConclusao: Locator = this.page.getByRole('listitem').filter({hasText: this.texto}).getByRole('checkbox');
+        await checkBoxConclusao.check();
     }
     
     async deletarTarefa(){
-        await this.tarefa.hover();
-        await this.botaoDeletar.click();
+        const tarefa : Locator = this.page.getByRole('listitem').filter({hasText: this.texto});
+        await tarefa.hover();
+        await tarefa.getByRole('button', {name: 'Delete'}).click();
     }
 }
