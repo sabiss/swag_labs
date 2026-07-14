49525e08050958739cd4955d2f2a2f222b9d3c11
sabiss <sabrinabezsilva@gmail.com>
2026-07-14 17:10:36 -0300

    REFACTOR: add condição pro teste de criação não ser inutilizado pela fixture

--- tests/tarefa.spec.ts
+++ tests/tarefa.spec.ts
@@ -1,23 +1,32 @@
 import { test, expect } from '@playwright/test';
 import { TodoPage } from '../pages/todoPage';
+import { TarefaComponent } from '../components/tarefaComponent';
 
 const mensagemDaTarefa: string = "Comprar leite";
+let todoPage : TodoPage;
+let tarefaCriada : TarefaComponent;
 
 test.describe('Suite de Testes de Tarefas', ()=>{
-    test.beforeEach(async ({ page }) => {
-        const todoPage = new TodoPage(page);
+    test.beforeEach(async ({ page }, testInfo) => {
+        todoPage = new TodoPage(page);
         await todoPage.acessarPagina();
+
+        //se não o teste de criar não vai servir pra verificar nada
+        if (testInfo.title === 'Criar tarefa') {
+            return; 
+        }
+
+        await todoPage.criarNovaTarefa(mensagemDaTarefa);
+        tarefaCriada = todoPage.obterTarefa(mensagemDaTarefa);
     });
 
     test('Criar tarefa', async({page})=>{
-        const todoPage: TodoPage = new TodoPage(page);
-
         await todoPage.criarNovaTarefa(mensagemDaTarefa);
-        const tarefaCriada = todoPage.obterTarefa(mensagemDaTarefa);
-
-        await expect(tarefaCriada.tarefa).toBeVisible()
-        await expect(tarefaCriada.tarefa).toHaveText(mensagemDaTarefa)
-        await expect(tarefaCriada.checkbox).not.toBeChecked()
+        const tarefa : TarefaComponent = todoPage.obterTarefa(mensagemDaTarefa);
+        
+        await expect(tarefa.tarefa).toBeVisible()
+        await expect(tarefa.tarefa).toHaveText(mensagemDaTarefa)
+        await expect(tarefa.checkbox).not.toBeChecked()
     })
 
     test('Deletar tarefa', async ({page}) => {
